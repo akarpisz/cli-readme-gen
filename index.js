34b671bf77c52adaa2b01c5c585b57ccd7c536f7
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-05-22 22:52:37 -0600

    application now linked to a cli command

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
+#!/usr/bin/env node
+
 const inquirer = require("inquirer");
 const{promisify} = require("util");
 const child = require("child_process");
 
 const promptAsync = promisify(inquirer.prompt);
 
-const tocList = [];
-// maybe do one function, askTOC and another, buildTOC
-//oh, and use async/await for that TOC stuff2
-
-async function tableCreate() {
-    try{
-    const confirmTable = await promptAsync([{
-    type: "confirm",
-    name: "includeTOC",
-    message: "Include a table of contents? (Recommended)",
-}])
-
-
-    
-    
-    if(includeTOC) {
-        console.log("worked a little");
-        
-        promptAsync([{
-        type: "editor",
-        name:"tocHeading",
-        message: "Enter Table of Contents heading:"
-        }])
-    }
-} catch(err) {
-    console.log(err);
-    
-}
-}    
-
-
-
-
-
-
-inquirer.prompt([
+const questions = [
     {
         name: "projectTitle",
         message: "Project Title?",
@@ -79,23 +46,59 @@ inquirer.prompt([
     {
         name: "githubEmail",
         message: "Github Email:"
-    }
-]).then( answer => {
-    console.log(answer);
-    //table of contents
-    //write file
+    }]
+
+let repo;
+const getRepo = () => {
     child.exec("git config --get remote.origin.url", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        } else{
-        let obj = stdout;
-        console.log(obj);
-        
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
     }
-})
-})
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    } else{
+    repo = stdout;
+    console.log(repo);
+}
+})}
+
+
+
+
+const tocList = [];
+// maybe do one function, askTOC and another, buildTOC
+//oh, and use async/await for that TOC stuff2
+
+async function tableCreate() {
+    try{
+    
+    const confirmTable = await promptAsync([{
+    type: "confirm",
+    name: "includeTOC",
+    message: "Include a table of contents? (Recommended)"
+}])
+    const addTableHeadings = await promptAsync([{
+        type: "editor",
+        name:"tocHeading",
+        message: "Enter Table of Contents heading with / in between:"
+        }])
+    console.log(addTableHeadings);
+    
+
+    
+} catch(err) {
+    console.log(err);
+    
+}
+}
+
+tableCreate()
+// inquirer.prompt(questions
+// ).then( answer => {
+//     console.log(answer);
+//     //table of contents
+//     //write file
+    
+// })
